b2e15d349306f25795e92bf2e0161dc77a954521
Opal <opal@localhost.localdomain>
2020-12-09 18:53:43 +0200

    package name

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core'
 
 export default {
-  id: 'com.fbmsger.fkfb',
+  id: 'com.fbweb.fkfb',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
